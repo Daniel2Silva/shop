b4da8ca99095001d8ccda1f711f329c4ae5666ae
Daniel Silva <danicater@hotmail.com>
2023-08-25 21:17:28 -0300

    melhorei muito o responsivo e coloquei o bentido menu hamburguer kkkkk

---
+++ script.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.menu-mobile')
+const nav = document.querySelector('.nav-itens')
+
+menu.addEventListener('click', () => {
+    nav.classList.toggle('ativo')
+})
+
+
+
+
