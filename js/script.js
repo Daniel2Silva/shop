5c2e55be89c5a48bf9d4ecba5e9168900986d4a4
Daniel Silva <danicater@hotmail.com>
2023-09-03 21:35:55 -0300

    animacoes com js, terminando a primeira pagin do site de vendas

---
+++ js/script.js
@@ -0,0 +1,10 @@
+import initMenu from "./modules/menu.js";
+import initScrollSuave from "./modules/scrollsuave.js";
+import initScrollAnimado from "./modules/animascroll.js";
+
+
+
+
+initScrollAnimado()
+initScrollSuave()
+initMenu()
