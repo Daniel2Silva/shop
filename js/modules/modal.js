2b4252d8d5d5849b502443147a0530567c44b39d
Daniel Silva <danicater@hotmail.com>
2023-10-06 01:11:20 -0300

    nova versao do site

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,17 +1,19 @@
-export default function initModal(){
+export default function initModal() {
+  const abrir = document.querySelector('[data-modal="conta"]');
+  const fechar = document.querySelector('[data-modal="fechar"]');
+  const modal = document.querySelector('[data-modal="modal"]');
 
+  function toggleModal() {
+    modal.classList.toggle("ativo");
+  }
+
+  function clickFora(event){
+    if(event.target === this){
+        toggleModal(event)
+    }
+  }
+
+  abrir.addEventListener("click", toggleModal);
+  fechar.addEventListener("click", toggleModal);
+  modal.addEventListener('click', clickFora)
 }
-const abrir = document.querySelector('[data-modal="conta"]')
-const fechar = document.querySelector('[data-modal="fechar"]')
-const modal = document.querySelector('[data-modal="modal"]')
-
-
-
-function ativarModal(){
-    modal.classList.toggle('ativo')
-};
-
-
-
-abrir.addEventListener('click', ativarModal);
-fechar.addEventListener('click', ativarModal);
