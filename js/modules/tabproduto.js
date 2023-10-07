8a893e8cfd0db28a3b367f83b740cc50124cc2ab
Daniel Silva <danicater@hotmail.com>
2023-10-06 23:10:11 -0300

    erro de js solucionado kkkk meu deus quase soquei a parede agora...

--- js/modules/tabproduto.js
+++ js/modules/tabproduto.js
@@ -1,22 +1,23 @@
-export default function tabProduto() {
+export default function initTabProduto() {
   const imgPequena = document.querySelectorAll(
     "[data-tab-produto='imagem-pequena'] img"
   );
   const imgGrande = document.querySelectorAll(
     "[data-tab-produto='imagem-grande'] img"
   );
+  if (imgPequena.length && imgGrande.length) {
+    imgGrande[0].classList.add("ativo");
+    function ativarTab(index) {
+      imgGrande.forEach((imagem) => {
+        imagem.classList.remove("ativo");
+      });
+      imgGrande[index].classList.add("ativo");
+    }
 
-  imgGrande[0].classList.add("ativo");
-  function ativarTab(index) {
-    imgGrande.forEach((imagem) => {
-      imagem.classList.remove("ativo");
+    imgPequena.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        ativarTab(index);
+      });
     });
-    imgGrande[index].classList.add("ativo");
   }
-
-  imgPequena.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      ativarTab(index);
-    });
-  });
 }
